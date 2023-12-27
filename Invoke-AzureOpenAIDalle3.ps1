15635a0a9479aad79600a6f133dbd5882eba8af3
Wojciech Napierała <voytass@op.pl>
2023-12-27 15:32:17 +0100

    🛠️ Invoke-AzureOpenAIDalle3.ps1 -> The changes are primarily focused on improving the readability of the code and increasing the robustness of the Azure API call. Notably, the code has been augmented with numerous comments that explain what each block of code does. Unused commented code lines have been removed. Also, it now includes functionality to handle potential errors in the API call using a try-catch block. Additionally, the implementation of the 'Show-Error' function has been extended with comments.

--- Invoke-AzureOpenAIDalle3.ps1
+++ Invoke-AzureOpenAIDalle3.ps1
@@ -8,69 +8,64 @@ function Invoke-AzureOpenAIDALLE3 {
         [string]$SavePath = [Environment]::GetFolderPath([Environment+SpecialFolder]::MyPictures)
     )
 
+    # Define the API version and Azure endpoint
     $apiVersion = "2023-12-01-preview"
     $azureEndpoint = "https://${serviceName}.openai.azure.com"
-    #$apiKey = "$env:AZURE_OPENAI_API_KEY"
-    
-
-    # Retrieve the prompt from the user
-    #$prompt = Read-Host "Enter your prompt for DALL-E 3 generation"
-
+        
+    # Create the JSON request body for the API call
     $requestBodyJSON = @{
         prompt = $prompt
         n      = 1
     } | ConvertTo-Json 
 
-    #$requestBody
-
+    # Define the headers for the API call
     $header = @{
         "Content-Type" = "application/json"
         "api-key"      = $ApiKey
     }
 
-    #$header
-
-    #endpoint: https://udtazureopenai.openai.azure.com/openai/deployments/Dalle3/images/generations?api-version=2023-12-01-preview
+    # Define the URI for the API call
     $URI = "$azureEndpoint/openai/deployments/$model/images/generations?api-version=$apiVersion"
-    #$URI = "$azureEndpoint/openai/"
-    #$URI
-    
+        
+    # Use a try-catch block to handle potential errors in the API call
     try {
+        # Make the API call
         $response = Invoke-RestMethod -Method Post -Uri $URI -Body $requestBodyJSON -Headers $header -TimeoutSec 30 -ErrorAction Stop
 
-        $response.error.code
-
-
-        #$imageData = $response.data
+        # Get the revised prompt and image URL from the response
         $imageRevisedPrompt = $response.data[0].revised_prompt
         $imageUrl = $response.data[0].url
 
+        # Display the revised prompt
         write-host $imageRevisedPrompt -ForegroundColor Cyan
 
+        # Define the filenames for the prompt and image
         $data = (get-date).ToString("yyyyMMddHHmmss")
         $PromptFileName = "$data.png.PROMPT.txt"
         $ImageFileName = "$data.png"
     
-        # FullNames
+        # Get the full paths for the prompt and image
         $promptFullName = Join-Path $SavePath $PromptFileName
         $ImageFullName = Join-Path $SavePath $ImageFileName
     
-        #save prompt
+        # Save the revised prompt to a file
         $imageRevisedPrompt | Add-Content -Path $promptFullName -Force
 
+        # Download the image and save it to a file
         Invoke-WebRequest -Uri $imageUrl -OutFile $ImageFullName
         
-        #show paths
+        # Display the paths of the saved files
         Write-Host $ImageFullName -ForegroundColor Blue
         Write-Host $promptFullName -ForegroundColor Blue
     }
     catch {
-        #Write-Host "Error getting image: $response.error"
+        # Display an error message if there was an issue with the API call
         Show-Error -ErrorMessage "Error getting image file: $($_.Exception.Message)"
     }
 
 }
 
+# Define a function to display and log errors
 function Show-Error {
     <#
 .SYNOPSIS
@@ -85,9 +80,10 @@ The error message to be displayed.
 
     param(
         [Parameter(Mandatory = $true)]
-        [string]$ErrorMessage
+        [string]$ErrorMessage # The error message to be displayed
     )
 
+    # Display the error message
     Write-Error $ErrorMessage
-    # Log error to file or other logging mechanism
+    # Log error to file or other logging mechanism (Not implemented in this function but can be added here)
 }
