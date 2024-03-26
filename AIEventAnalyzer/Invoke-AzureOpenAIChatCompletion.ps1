91e167635b30156e0eef5932d8c95ea3255e62bb
Wojciech Napierała <voytass@op.pl>
2024-03-26 16:37:14 +0100

    🛠️ Invoke-AzureOpenAIChatCompletion.ps1 -> Added Clear-AzureOpenAIAPIEnv function.

--- AIEventAnalyzer/Invoke-AzureOpenAIChatCompletion.ps1
+++ AIEventAnalyzer/Invoke-AzureOpenAIChatCompletion.ps1
@@ -890,22 +890,33 @@ function Invoke-AzureOpenAIChatCompletion {
         return $VariableValue
     }
     
+    <#
+    .SYNOPSIS
+    This function clears the Azure OpenAI API environment variables.
+
+    .DESCRIPTION
+    The Clear-AzureOpenAIAPIEnv function clears the values of the Azure OpenAI API environment variables. If an error occurs during the process, it provides an error message to the user.
+
+    .EXAMPLE
+    Clear-AzureOpenAIAPIEnv
+    #>
     function Clear-AzureOpenAIAPIEnv {
         param()
         try {
+            # Clear the environment variables related to Azure OpenAI API
             [System.Environment]::SetEnvironmentVariable("API_AZURE_OPENAI_APIVERSION", "", "User")
             [System.Environment]::SetEnvironmentVariable("API_AZURE_OPENAI_DEPLOYMENT", "", "User")
             [System.Environment]::SetEnvironmentVariable("API_AZURE_OPENAI_KEY", "", "User")
             [System.Environment]::SetEnvironmentVariable("API_AZURE_OPENAI_Endpoint", "", "User")
+            
+            # Inform the user about the successful deletion of the environment variables
             Write-Host "Environment variables for Azure API have been deleted successfully."
         }
         catch {
+            # Inform the user about any errors occurred during the deletion of the environment variables
             Write-Host "An error occurred while trying to delete Azure API environment variables. Please check your permissions and try again."
         }
     }
-    
-
-    # Main program 
 
     # Define constants for environment variable names
     $API_AZURE_OPENAI_APIVERSION = "API_AZURE_OPENAI_APIVERSION"
