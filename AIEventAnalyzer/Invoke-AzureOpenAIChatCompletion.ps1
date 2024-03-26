bd6de4ae6346e7d4013f865c3beea24be39eee10
Wojciech Napierała <voytass@op.pl>
2024-03-26 16:25:46 +0100

    🛠️ Invoke-AzureOpenAIChatCompletion.ps1 -> Added comments/documentation for `Format-Message` function.

--- AIEventAnalyzer/Invoke-AzureOpenAIChatCompletion.ps1
+++ AIEventAnalyzer/Invoke-AzureOpenAIChatCompletion.ps1
@@ -815,13 +815,27 @@ function Invoke-AzureOpenAIChatCompletion {
         Write-Output $ErrorVar
     }
 
+    <#
+    .SYNOPSIS
+    This function formats the provided message by removing non-ASCII characters.
+
+    .DESCRIPTION
+    The Format-Message function takes in a string message and returns a version of the message with all non-ASCII characters removed. 
+    This can be used to ensure that the message can be properly displayed in environments that only support ASCII characters.
+
+    .PARAMETER Message
+    The string message to be formatted. This parameter is mandatory.
+
+    .EXAMPLE
+    $userMessage = Format-Message -Message $OneTimeUserPrompt
+    #>
     function Format-Message {
         param(
             [Parameter(Mandatory = $true)]
             [string]$Message
         )
-        # Usage:
-        #$userMessage = Format-Message -Message $OneTimeUserPrompt
+
+        # Remove non-ASCII characters from the message
         return [System.Text.RegularExpressions.Regex]::Replace($Message, "[^\x00-\x7F]", "")
     }
 
